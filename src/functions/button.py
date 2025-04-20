51e45a82f2f6ba544057528ca29ef1ad09d6ac11
imsergio <uhmarquez7@gmail.com>
2025-04-19 20:10:21 -0500

    Restructure GUI layout and add cleanup button, creation of a Frame to group buttons.

--- src/functions/button.py
+++ src/functions/button.py
@@ -1,7 +1,7 @@
-# Relative import option
-from clases.Transform import Transform_word
+"""Módulo para el botón"""
+import clases.Transform
 
-def button_clicked(entry, list_word, app):
+def button_send(entry, list_word, app):
     """Convertir palabra a alfabeto fonético"""
     word = entry.get().upper()
     list_word.delete(0, "end")
@@ -11,3 +11,8 @@ def button_clicked(entry, list_word, app):
             list_word.insert(index, element)
     else:
         list_word.insert(1,"Escribe solo letras")
+
+def button_clean(entry, list_word):
+    """Eliminar contenido de la caja de texto"""
+    entry.delete(0, "end")
+    list_word.delete(0, "end")
--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 """GUI PROGRAM"""
 
 #importamos la librerías
-from tkinter import Tk, Button, Label, Entry,Listbox, PhotoImage
+from tkinter import Tk, Frame, Button, Label, Entry,Listbox, PhotoImage
 from clases import Transform
 from functions import button
 
+#creamos el objeto
 app = Transform.Transform_word()
 
+#creamos la ventana
 window = Tk()
+
+#configuramos la ventana
 window.title("Alfabeto fonético Nato")
 window.geometry("300x420")
 window.minsize(300, 300)
@@ -17,13 +21,24 @@ window.config(padx=20, pady=50)
 imagen = PhotoImage(file="assets/img/nato.png")
 imagen_label = Label(window, image=imagen)
 imagen_label.pack(side="top", pady=10)
+
 #creamos la caja de texto
-entry = Entry(window, font=("Arial", 10, "italic"),width=40)
+entry = Entry(window, font=("Arial", 10, "italic"),width=30)
 entry.pack(side="top", pady=10)
-#creamos el botón
-btn = Button(window, text="Convertir", font=("Arial", 10),
+
+#creamos el frame
+frame = Frame(window)
+frame.pack(side="top", pady=10)
+
+#creamos el botón para enviar la palabra
+btn_send = Button(frame, text="Convertir", font=("Arial", 10),
             command=lambda: button.button_send(entry, list_word, app))
-btn.pack(side="top", pady=10)
+btn_send.pack(side="left", pady=10, padx=10)
+#creamos el botón para limpiar la caja de texto
+btn_clean = Button(frame, text="Limpiar", font=("Arial", 10),
+            command=lambda: button.button_clean(entry, list_word))
+btn_clean.pack(side="right", pady=10)
+
 #creamos la lista
 list_word = Listbox(window, font=("Arial", 10))
 list_word.pack(side="top", pady=10)
