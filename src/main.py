f675df394520a90dd63bf448702f57d2b5cd3379
imsergio <uhmarquez7@gmail.com>
2025-04-19 21:11:36 -0500

    Improve layout and add scrollbar to listbox.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 """GUI PROGRAM"""
 
 #importamos la librerías
-from tkinter import Tk, Frame, Button, Label, Entry,Listbox, PhotoImage
+from tkinter import Tk, Frame, Button, Label, Entry,Listbox, PhotoImage,Scrollbar
+
 from clases import Transform
 from functions import button
 
@@ -22,28 +23,40 @@ imagen = PhotoImage(file="assets/img/nato.png")
 imagen_label = Label(window, image=imagen)
 imagen_label.pack(side="top", pady=10)
 
-#creamos la caja de texto
-entry = Entry(window, font=("Arial", 10, "italic"),width=30)
+#creamos la caja de texto y establecemos el foco en la entrada de texto al abrir la aplicación
+entry = Entry(window, font=("Arial", 10, "italic"),justify="center",width=30)
+entry.focus() 
 entry.pack(side="top", pady=10)
 
 # Asociar la función a la tecla Enter
 entry.bind("<Return>", lambda event: button.button_send(entry, list_word, app))
 
-#creamos el frame
-frame = Frame(window)
-frame.pack(side="top", pady=10)
+#creamos el frame para los botones
+frame1 = Frame(window)
+frame1.pack(side="top", pady=10)
 
 #creamos el botón para enviar la palabra
-btn_send = Button(frame, text="Convertir", font=("Arial", 10),
+btn_send = Button(frame1, text="Convertir", font=("Arial", 10),
             command=lambda: button.button_send(entry, list_word, app))
 btn_send.pack(side="left", pady=10, padx=10)
 #creamos el botón para limpiar la caja de texto
-btn_clean = Button(frame, text="Limpiar", font=("Arial", 10),
+btn_clean = Button(frame1, text="Limpiar", font=("Arial", 10),
             command=lambda: button.button_clean(entry, list_word))
 btn_clean.pack(side="right", pady=10)
 
+#creamos el frame para la lista
+frame2 = Frame(window)
+frame2.pack(side="top", pady=10)
+
+scrollbar = Scrollbar(frame2)
+scrollbar.pack(side="right", fill="y" )
+
 #creamos la lista
-list_word = Listbox(window, font=("Arial", 10))
-list_word.pack(side="top", pady=10)
+list_word = Listbox(frame2, yscrollcommand = scrollbar.set, font=("Arial", 10),exportselection=True,selectmode="extended",bd=1)
+#configuramos la lista
+scrollbar.config( command = list_word.yview )
+#mostramos la lista
+list_word.pack(side="top",fill ="both", pady=10)
+
 
 window.mainloop()
