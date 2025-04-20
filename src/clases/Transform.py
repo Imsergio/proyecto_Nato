febc2045750c619c82a97bf58202055c31af00c3
imsergio <uhmarquez7@gmail.com>
2025-04-19 19:01:14 -0500

    Add class for reading and transform data, move data file to assets folder

---
+++ src/clases/Transform.py
@@ -0,0 +1,38 @@
+"""CLASE CONVERTIR DATOS ALFABETO FONETICO"""
+#Importar librerias
+import pandas as pd
+
+#declarar clase
+class Transform_word:
+    """Convierte una palabra en su equivalente en el alfabeto fonético de la OTAN
+    
+    param: palabra: palabra a convertir
+    param: diccionario: diccionario con las letras y sus equivalentes en el alfabeto fonético de la OTAN
+    param: salida: lista con las letras convertidas
+    """
+    def __init__(self):
+        """Inicializa la clase"""
+        self.word = ""
+        self.dict = ""
+        self.output = []
+
+    def read_data(self, path="assets/data/nato_phonetic_alphabet.csv"):
+        """Obtiene los datos del archivo csv y los guarda en un diccionario"""
+        self.dict = pd.read_csv(path)
+        self.dict = {row.letter:row.code for (index,row) in self.dict.iterrows()}
+        return self.dict
+
+    def transform_word_dict(self, word, phonetic_dict):
+        """Convierte una palabra en su equivalente en el alfabeto fonético de la OTAN"""
+        self.word = word
+        self.dict = phonetic_dict
+        self.output = [f"{i+1}. {self.dict[letra]}" for i, letra in enumerate(self.word)]
+        return self.output 
+
+# app = Convertir()
+# print(app.convertir_palabra_dict("HELLO", app.leer_datos("assets/data/nato_phonetic_alphabet.csv")))
+
+
+
+
+
